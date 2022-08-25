49269b0868e6196c682f098a60fa100d1a763763
Armando <arandaae19@gmail.com>
2022-08-25 08:23:17 -0500

    Add project files.

---
+++ RoutingDemo/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoutingDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return Content("Home");
+        }
+
+        [Route("About")]
+        public IActionResult About()
+        {
+            return Content("About");
+        }
+
+        public IActionResult Display (string id)
+        {
+            if (id == null)
+                return Content("No ID supplied");
+            else
+                return Content("Id is " + id);
+        }
+
+        [Route("[action]/{start}/{end?}/{message?}")] //The arguments in the route and in the method must be the same
+        public IActionResult Countdown(int start, int end = 0, string messege = "")
+        {
+            string contentString = "Counting down: \n";
+            for (int i = start; i >= end; i--)
+            {
+                contentString += i + "\n";
+            }
+            contentString += messege;
+            return Content(contentString);
+        }
+    }
+}
---
+++ RoutingDemo/Controllers/Product.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoutingDemo.Controllers
+{
+    public class Product : Controller
+    {
+        public IActionResult List(string cat, int num)
+        {
+            return Content("Product Controller, List Action, " + 
+                "Category = " + cat + ", Page = " + num);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            return Content("Product Controller, Detail Action, ID = " + id);
+        }
+    }
+}
